9591d378a3c0507b05930f65ec6349fa7de9a6ad
Leo Macedo <leomacedo19@gmail.com>
2026-03-24 07:52:45 -0300

    Fix: Melhoria na validação de e-mail usando Regex

--- dia1_email.py
+++ dia1_email.py
@@ -1,10 +1,17 @@
-email = input("Digite um email: ")
+import re
 
-# Limpa os espaços e já converte tudo para minúsculo na mesma linha!
-email = email.strip().lower()
+email = input("Digite um email: ").strip().lower()
 
-if "@" in email and "." in email:
+# O padrão Regex para um e-mail básico:
+# 1. ^[a-z0-9_.-]+ -> Começa com letras, números, _ . ou -
+# 2. @ -> Tem que ter o @ no meio
+# 3. [a-z0-9.-]+ -> Depois do @ tem domínio (ex: gmail)
+# 4. \.[a-z]+$ -> Tem que terminar com um ponto e letras (ex: .com)
+padrao_email = r"^[a-z0-9_.-]+@[a-z0-9.-]+\.[a-z]+$"
+
+# A função re.match() tenta encaixar o email digitado nesse padrão
+if re.match(padrao_email, email):
     print("Email válido")
     print(f"Email lido: {email}")
-else:    
+else:
     print("Email inválido")
