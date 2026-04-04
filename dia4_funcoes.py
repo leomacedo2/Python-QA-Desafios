9e3d22965f2c26efc3cfdff3348880d10a3b5758
Leo Macedo <leomacedo19@gmail.com>
2026-04-04 10:01:51 -0300

    feat: Código dia 4 pronto. Prática de funções em Python e Regex.

---
+++ dia4_funcoes.py
@@ -0,0 +1,17 @@
+import re
+
+def validar_forca_senha(senha):
+    tamanho_ok = len(senha) >= 8
+    tem_maiuscula = re.search(r"[A-Z]", senha)
+    tem_minuscula = re.search(r"[a-z]", senha)
+    tem_numero = re.search(r"[0-9]", senha)
+    tem_especial = re.search(r"[!@#$%^&*(),.?\":{}|<>]", senha)
+    
+    if tamanho_ok and tem_maiuscula and tem_minuscula and tem_numero and tem_especial:
+        return True
+    else:
+        return False
+
+
+print(validar_forca_senha("senha123"))
+print(validar_forca_senha("Forte@2026"))
