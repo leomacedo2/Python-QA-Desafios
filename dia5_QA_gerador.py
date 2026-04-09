82b8e13d66d40a6cd695127ae4b4c70493014890
Leonardo <leomacedo19@gmail.com>
2026-04-09 17:56:55 -0300

    feat: Código para asserts em QA do dia 5

---
+++ dia5_QA_gerador.py
@@ -0,0 +1,15 @@
+# Código para treinar teste de aleatoriedade.
+
+from dia5_gerador_dados import gerar_email_fake
+
+import re
+
+padrao_email = r"^[a-z]+[0-9]{4}@testeqa\.com$"
+
+for i in range(1, 101):
+    # A cada volta do laço, acionamos a função para gerar um NOVO email
+    email_teste = gerar_email_fake()
+
+    assert re.match(padrao_email, email_teste)
+
+print("\nTodos os testes passaram! ✅")
